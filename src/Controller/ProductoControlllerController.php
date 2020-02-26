4a06a865b099bc4c612ca30c7260be3c32db8343
devopshogarth <>
2020-02-26 17:33:30 -0300

    Eliminar y editar productos

--- src/Controller/ProductoControlllerController.php
+++ src/Controller/ProductoControlllerController.php
@@ -2,28 +2,18 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
+ 
 use App\Entity\Producto; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route; 
 use App\FormCrear\PostType; 
 use Symfony\Component\HttpFoundation\Request;  
-=======
-use App\Entity\Producto;
-use App\Repository\ProductoRepository; 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
->>>>>>> 6cd9971a82d997e1455baeb0ebb657870f7b0e4d
-
+ 
 
 
 class ProductoControlllerController extends AbstractController  
 {
     /**
-<<<<<<< HEAD
      * @Route("/productos", name="producto_controlller")
      * Productos habilitados
      */
@@ -46,36 +36,11 @@ class ProductoControlllerController extends AbstractController
      * @Route("/productos/crear", name="v_crear_producto")
      */
     public function v_crear_producto(Request $request)
-=======
-     * @Route("/productos", name="productos_controlller")
-     */
-    public function index_producto()
-    {   
-        $productos = $this->getDoctrine()
-            ->getRepository(Producto::class)
-            ->findBy([
-                'enabled' => 1 
-            ]);
-
-        var_dump($productos);
-            
-        return $this->render('producto_controlller/index.html.twig', [
-            'controller_name' => 'ProductoControlllerController',
-            'productos' => $productos
-        ]);
-    }
-
-    /**
-     * @Route("/producto/crear", name="create_product")
-     */
-    public function createProduct(ValidatorInterface $validator): Response
->>>>>>> 6cd9971a82d997e1455baeb0ebb657870f7b0e4d
     {
-         $producto = new Producto();
+        $producto = new Producto();
         
         $form = $this->createForm(PostType::class, $producto);
 
-<<<<<<< HEAD
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) 
@@ -103,9 +68,82 @@ class ProductoControlllerController extends AbstractController
         
     }
     
+    /**
+     * @Route("/productos/editar/{id}", name="v_editar_producto")
+     */
+    public function v_editar_producto( Request $request, $id )
+    {   
+        $producto = new Producto();
+        $form = $this->createForm(PostType::class, $producto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {   
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            $task = $form->getData();
+
+            var_dump($task->getNombre());
+
+            $producto = $entityManager->getRepository(Producto::class)->find($id);
+
+            $producto->setNombre($task->getNombre());
+            $producto->setDescripcion($task->getDescripcion());
+            $producto->setPrecioCosto($task->getPrecioCosto());
+            $producto->setPrecioVenta($task->getPrecioVenta());
+            $producto->setStock($task->getStock());
+            $producto->setCodigo($task->getCodigo());
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('producto_controlller');
+
+            /*$producto->setName('New product name!');*/
 
 
-   
+
+            /*
+            $task = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('producto_controlller');*/
+            //echo "UPDATE";
+        }
+        else
+        {
+
+            $repository = $this->getDoctrine()->getRepository(Producto::class);
+            $producto = $repository->findOneBy(['id' => $id]);
+            $form = $this->createForm(PostType::class, $producto);
+            return $this->render('producto_controlller/editar.html.twig', array(
+                'id' => $id,
+                'form' => $form->createView()
+            ));        
+        }
+    }
+
+/**
+     * @Route("/productos/eliminar/{id}", name="eliminar_producto")
+     */
+    public function eliminar_producto( Request $request, $id )
+    {   
+        $entityManager = $this->getDoctrine()->getManager();
+        $producto = $entityManager->getRepository(Producto::class)->find($id);
+        
+        if (!$producto) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $entityManager->remove($producto);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('producto_controlller');
+    }
+
     /**
      * @Route("/productos/traer", name="json_producto")
      * Productos JSON
@@ -124,94 +162,3 @@ class ProductoControlllerController extends AbstractController
 
 
 }   
-=======
-        $producto = new Producto();
-        $producto->setNombre("aaaa");
-        $producto->setPrecioCosto(150);
-        $producto->setStock(10);
-
-
-        $errors = $validator->validate($producto);
-        
-        if (count($errors) > 0) {
-            return new Response((string) $errors, 400);
-        }
-        else
-        {
-
-            // tell Doctrine you want to (eventually) save the producto (no queries yet)
-            $entityManager->persist($producto);
-
-            // actually executes the queries (i.e. the INSERT query)
-            $entityManager->flush();
-            return new Response('Saved new product with id '.$producto->getId());
-        }
-
-        
-    }
-
-    /**
-     * @Route("/producto/ver/{id}", name="product_show")
-     */
-    public function show($id)
-    {
-        $producto = $this->getDoctrine()
-            ->getRepository(Producto::class)
-            ->find($id);
-
-        if (!$producto) {
-            throw $this->createNotFoundException(
-                'No producto found for id '.$id
-            );
-        }
-
-        return new Response('Check out this great producto: '.$producto->getNombre());
- 
-    }
-
-    /**
-     * @Route("/producto/actualizar/{id}", name="product_update")
-     */
-    public function update($id)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $producto = $entityManager->getRepository(Producto::class)->find($id);
-
-        if (!$producto) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        $producto->setNombre('New AAAA name!');
-        $entityManager->flush();
-
-        return $this->redirectToRoute('product_show', [
-            'id' => $producto->getId()
-        ]);
-    }
-
-    /**
-     * @Route("/producto/delete/{id}", name="delete  _update")
-     */
-    public function delete($id)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $producto = $entityManager->getRepository(Producto::class)->find($id);
-
-        if (!$producto) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        $entityManager->remove($producto);
-        $entityManager->flush();
-        
-        return new Response('Eliminado exitosamente' );
-
-    }
-
- 
-}
->>>>>>> 6cd9971a82d997e1455baeb0ebb657870f7b0e4d
