7fdc1c6d899945e707772af462fb5af6192f1d9e
amaglio <adrian.magliola@gmail.com>
2020-02-27 22:32:40 -0300

    Relacion producto y ventas

--- src/Migrations/Version20200227235630.php
+++ src/Migrations/Version20200227235630.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200217134847 extends AbstractMigration
+final class Version20200227235630 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,9 @@ final class Version20200217134847 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        //$this->addSql('ALTER TABLE producto ADD enabled TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE producto_venta (id INT AUTO_INCREMENT NOT NULL, producto_id_id INT NOT NULL, venta_id_id INT NOT NULL, cantidad INT NOT NULL, INDEX IDX_CFC0E63F3F63963D (producto_id_id), INDEX IDX_CFC0E63F9F1AB70D (venta_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE producto_venta ADD CONSTRAINT FK_CFC0E63F3F63963D FOREIGN KEY (producto_id_id) REFERENCES producto (id)');
+        $this->addSql('ALTER TABLE producto_venta ADD CONSTRAINT FK_CFC0E63F9F1AB70D FOREIGN KEY (venta_id_id) REFERENCES venta (id)');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +32,6 @@ final class Version20200217134847 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE producto DROP enabled');
+        $this->addSql('DROP TABLE producto_venta');
     }
 }
