0c9fa3c1752e58f6a7b507866271f10571d2b02a
amaglio <adrian.magliola@gmail.com>
2020-04-02 19:51:23 -0300

    Template crear producto

--- src/AppBundle/Form/Type/FormProducto.php
+++ src/AppBundle/Form/Type/FormProducto.php
@@ -15,15 +15,29 @@ class PostType extends AbstractType
     {
 
         $builder
-            ->add('nombre', TextType::class)
-            ->add('codigo', TextType::class)
-            ->add('descripcion', TextareaType::class)
-            ->add('precio_costo', TextType::class)
-            ->add('precio_venta', TextType::class)
-            ->add('stock', TextType::class) 
+            ->add('nombre', TextType::class, [
+                'label' => 'Nombre y apellido',
+                'attr' => ['class' => 'col-md-8 form-control'],
+            ])
+            ->add('codigo', TextType::class, [
+                'label' => 'Codigo',
+                'attr' => ['class' => 'col-md-8 form-control'],
+            ])
+            ->add('descripcion', TextareaType::class, [ 
+                'attr' => ['class' => 'col-md-8 form-control']
+            ])
+            ->add('precio_costo', TextType::class,[ 
+                'attr' => ['class' => 'col-md-8 form-control']
+            ])
+            ->add('precio_venta', TextType::class, [ 
+                'attr' => ['class' => 'col-md-8 form-control']
+            ])
+            ->add('stock', TextType::class, [ 
+                'attr' => ['class' => 'col-md-8 form-control']
+            ]) 
             ->add('submit', SubmitType::class, [
                 'label' => 'Guardar',
-                'attr' => ['class' => 'btn btn-primary'],
+                'attr' => ['class' => 'btn btn-info text-center'],
             ]); 
     }
 
